368c7792453ac02e405821386fefc5aa99928ac4
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-25 18:51:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-    array.insert(array.size - 1,"and ")
+    insert_and = array[-1]
+    insert_and.insert(0,"and ")
+    array.join(", ")
   end
 end
